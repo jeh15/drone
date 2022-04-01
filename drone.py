5902e1cbcbce9b8c2017a47f6c7e7beaf6a580aa
Jacob Hackett <jeh15@my.fsu.edu>
2022-04-01 17:24:23 -0400

    Updating drone class.

--- drone.py
+++ drone.py
@@ -1,17 +1,19 @@
-class Drone:
+import pdb
+
+import numpy as np
+
+
+class Drone(object):
+    numpy = __import__('numpy')
+    sympy = __import__('sympy')
+    osqp = __import__('osqp')
+    scipy = __import__('scipy')
+    pdb = __import__('pdb')
 
     def __init__(self, nodes, x0, desired_trajectory, **kwargs):
         """
         Initialize Attributes:
         """
-
-        # Import Class Specific Libraries:
-        import numpy
-        import sympy
-        import osqp
-        import scipy
-        from scipy.sparse import csc_matrix
-        
         # Set Attributes:
         self.nodes = nodes
         self.reference_trajectory = desired_trajectory
@@ -24,21 +26,21 @@ class Drone:
         """
 
         # Symbolic Variables:
-        _x = sympy.symarray('_x', self.nodes)
-        _dx = sympy.symarray('_dx', self.nodes)
-        _y = sympy.symarray('_y', self.nodes)
-        _dy = sympy.symarray('_dy', self.nodes)
-        _f_x = sympy.symarray('f_x', self.nodes)
-        _f_y = sympy.symarray('f_y', self.nodes)
+        _x = self.sympy.symarray('_x', self.nodes)
+        _dx = self.sympy.symarray('_dx', self.nodes)
+        _y = self.sympy.symarray('_y', self.nodes)
+        _dy = self.sympy.symarray('_dy', self.nodes)
+        _f_x = self.sympy.symarray('f_x', self.nodes)
+        _f_y = self.sympy.symarray('f_y', self.nodes)
 
         # Symbolic Variables representing Reference/Desired trajectory:
-        _x_reference = sympy.symarray('x_reference', self.nodes)
-        _dx_reference = sympy.symarray('dx_reference', self.nodes)
-        _y_reference = sympy.symarray('y_reference', self.nodes)
-        _dy_reference = sympy.symarray('dy_reference', self.nodes)
+        _x_reference = self.sympy.symarray('x_reference', self.nodes)
+        _dx_reference = self.sympy.symarray('dx_reference', self.nodes)
+        _y_reference = self.sympy.symarray('y_reference', self.nodes)
+        _dy_reference = self.sympy.symarray('dy_reference', self.nodes)
 
         # Design Variable Vector:
-        _z = numpy.concatenate([_x, _dx, _f_x, _y, _dy, _f_y], axis=0)
+        _z = self.numpy.concatenate([_x, _dx, _f_x, _y, _dy, _f_y], axis=0)
 
         # Commonly used numbers:
         _number_of_states = 4
@@ -48,12 +50,12 @@ class Drone:
         _design_vector_column_format = (self.nodes, _number_of_design_variables)
 
         # Reference/Desired Variable Vector:
-        _reference_trajectory = numpy.concatenate([_x_reference, _dx_reference, _y_reference, _dy_reference], axis=0)
+        _reference_trajectory = self.numpy.concatenate([_x_reference, _dx_reference, _y_reference, _dy_reference], axis=0)
 
         # Objective Function:
         _objective_function = ((_x - _x_reference) ** 2 + (_y - _y_reference) ** 2 + (_dx - _dx_reference) ** 2 
                         + (_dy - _dy_reference) ** 2 + (_f_x ** 2 + _f_y ** 2))
-        _objective_function = numpy.sum(_objective_function)
+        _objective_function = self.numpy.sum(_objective_function)
 
         # Compute Hessian:
         _H = [[_objective_function.diff(_axis_0).diff(_axis_1) for _axis_0 in _z] for _axis_1 in _z]
@@ -62,17 +64,17 @@ class Drone:
         _f = [_objective_function.diff(_axis_0) for _axis_0 in _z]
         _f = [_f[_i].subs(_z[_i], 0) for _i in range(_design_vector_length)]
 
-        # Convert H to numpy array:
-        _H = numpy.asarray(_H, dtype=float)
-        self.H = scipy.sparse.csc_matrix(_H)
+        # Convert H to self.numpy array:
+        _H = self.numpy.asarray(_H, dtype=float)
+        self.H = self.scipy.sparse.csc_matrix(_H)
 
         # Lambdify f:
-        self.f = staticmethod(sympy.lambdify([_reference_trajectory], _f, 'numpy'))
+        self.f = staticmethod(self.sympy.lambdify([_reference_trajectory], _f, 'numpy'))
 
         # States:
-        _q = numpy.stack([_x, _y], axis=1)
-        _dq = numpy.stack([_dx, _dy], axis=1)
-        _u = numpy.stack([_f_x, _f_y], axis=1)
+        _q = self.numpy.stack([_x, _y], axis=1)
+        _dq = self.numpy.stack([_dx, _dy], axis=1)
+        _u = self.numpy.stack([_f_x, _f_y], axis=1)
 
         # Planning Horizon:
         _planning_horizon = 1
@@ -82,25 +84,24 @@ class Drone:
         # Dynamics Constraints:
         _dynamics_position = _q[1:, :] - _q[:-1, :] - _dq[:-1, :] * self.dt
         _dynamics_velocity = _dq[1:, :] - _dq[:-1, :] - _u[:-1, :] * self.dt
-        _collocation_constraints = numpy.stack((_dynamics_position, _dynamics_velocity), axis=0).flatten(order='F')
-        _A_collocation, _b_collocation = sympy.linear_eq_to_matrix(_collocation_constraints, _z)
+        _collocation_constraints = self.numpy.stack((_dynamics_position, _dynamics_velocity), axis=0).flatten(order='F')
+        _A_collocation, _b_collocation = self.sympy.linear_eq_to_matrix(_collocation_constraints, _z)
+        _b_collocation = self.numpy.array(_b_collocation).astype(self.numpy.float64)
         self._b_collocation = _b_collocation.flatten(order='F')
-        self._A_collocation = scipy.sparse.csc_matrix(_A_collocation, dtype=float)
+        self._A_collocation = self.scipy.sparse.csc_matrix(_A_collocation, dtype=float)
 
         # Initial Conditions Constraints:
-        _initial_condition = sympy.symarray('initial_condition', _number_of_states)
-        _initial_conditions = numpy.concatenate(
+        _initial_condition = self.sympy.symarray('initial_condition', _number_of_states)
+        _initial_conditions = self.numpy.concatenate(
                         (_q[0, :] - _initial_condition[:2], _dq[0, :] - _initial_condition[2:])).flatten(order='F')
-        _A_initial_conditions, _b_initial_conditions = sympy.linear_eq_to_matrix(_initial_conditions, _z)
-        _b_initial_conditions = _b_initial_conditions.flatten(order='F')
-        self._A_initial_conditions = scipy.sparse.csc_matrix(_A_initial_conditions, dtype=float)
-        self.initial_condition_constraint = staticmethod(
-                        sympy.lambdify([_initial_condition], _b_initial_conditions, 'numpy'))
+        _A_initial_conditions, _b_initial_conditions = self.sympy.linear_eq_to_matrix(_initial_conditions, _z)
+        self._A_initial_conditions = self.scipy.sparse.csc_matrix(_A_initial_conditions, dtype=float)
+        self.initial_condition_constraint = staticmethod(self.sympy.lambdify([_initial_condition], _b_initial_conditions, 'numpy'))
 
         # Inequality Constraints:
         # Variable Bounds:
         # Pre-allocate Matrix:
-        _design_variable_bound = numpy.zeros(_design_vector_column_format)
+        _design_variable_bound = self.numpy.zeros(_design_vector_column_format)
         # Defaults to Symmetric Bounds:
         position_bound = 10
         velocity_bound = 10
@@ -113,8 +114,8 @@ class Drone:
         _design_variable_bound[:, 5] = force_bound
         _design_variable_bound = _design_variable_bound.flatten(order='F')
         _design_variable_bound = _z[:] - _design_variable_bound
-        _A_variable_bounds, _ = sympy.linear_eq_to_matrix(_design_variable_bound, _z)
-        self._A_variable_bounds = scipy.sparse.csc_matrix(_A_variable_bounds, dtype=float)
+        _A_variable_bounds, _ = self.sympy.linear_eq_to_matrix(_design_variable_bound, _z)
+        self._A_variable_bounds = self.scipy.sparse.csc_matrix(_A_variable_bounds, dtype=float)
         self._design_variable_bound = _design_variable_bound
 
         # Initialize QP:
@@ -123,9 +124,9 @@ class Drone:
         self.q = self.f(self.reference_trajectory)
 
         # Updates Equality Constraints:
-        _A_equality = scipy.sparse.vstack([self._A_collocation, self._A_initial_conditions])
+        _A_equality = self.scipy.sparse.vstack([self._A_collocation, self._A_initial_conditions])
         _b_initial_conditions = self.initial_condition_constraint(self.x0)
-        _lower_equality = numpy.concatenate([self._b_collocation, _b_initial_conditions], axis=0)
+        _lower_equality = self.numpy.concatenate([self._b_collocation, _b_initial_conditions], axis=0)
         _upper_equality = _lower_equality
 
         # Update Inequality Constraints:
@@ -134,12 +135,12 @@ class Drone:
         _upper_inequality = self._design_variable_bound
 
         # Combine Constraints:
-        self.A = scipy.sparse.vstack([_A_equality, _A_inequality])
-        self.l = numpy.concatenate([_lower_equality, _lower_inequality], axis=0)
-        self.u = numpy.concatenate([_upper_equality, _upper_inequality], axis=0)
+        self.A = self.scipy.sparse.vstack([_A_equality, _A_inequality])
+        self.l = self.numpy.concatenate([_lower_equality, _lower_inequality], axis=0)
+        self.u = self.numpy.concatenate([_upper_equality, _upper_inequality], axis=0)
 
         # Create QP Object:
-        self.qp = osqp.OSQP()
+        self.qp = self.osqp.OSQP()
         self.qp.setup(self.H, self.q, self.A, self.l, self.u, warm_start=True)
 
 
@@ -153,9 +154,9 @@ class Drone:
         self.q = self.f(self.reference_trajectory)
 
         # Updates Equality Constraints:
-        _A_equality = scipy.sparse.vstack([self._A_collocation, self._A_initial_conditions])
+        _A_equality = self.scipy.sparse.vstack([self._A_collocation, self._A_initial_conditions])
         _b_initial_conditions = self.initial_condition_constraint(self.x0)
-        _lower_equality = numpy.concatenate([self._b_collocation, _b_initial_conditions], axis=0)
+        _lower_equality = self.numpy.concatenate([self._b_collocation, _b_initial_conditions], axis=0)
         _upper_equality = _lower_equality
 
         # Update Inequality Constraints:
@@ -164,9 +165,9 @@ class Drone:
         _upper_inequality = self._design_variable_bound
 
         # Combine Constraints:
-        self.A = scipy.sparse.vstack([_A_equality, _A_inequality])
-        self.l = numpy.concatenate([_lower_equality, _lower_inequality], axis=0)
-        self.u = numpy.concatenate([_upper_equality, _upper_inequality], axis=0)
+        self.A = self.scipy.sparse.vstack([_A_equality, _A_inequality])
+        self.l = self.numpy.concatenate([_lower_equality, _lower_inequality], axis=0)
+        self.u = self.numpy.concatenate([_upper_equality, _upper_inequality], axis=0)
 
         self.qp.update()
 
--- drone_test.py
+++ drone_test.py
@@ -1,9 +1,25 @@
-import Drone
 import numpy
+import sympy
+import osqp
+import scipy
+from scipy.sparse import csc_matrix
+from matplotlib import pyplot as plt
+import pdb
+
+import drone
 
 nodes = 21
 number_of_states = 5
 x0 = numpy.zeros((nodes * number_of_states, 1))
 desired_trajectory = numpy.concatenate((numpy.ones((nodes,)), numpy.zeros((nodes,)), numpy.ones((nodes,)), numpy.zeros((nodes,))), axis=0)
 
-drone = Drone(nodes, x0, desired_trajectory)
+agent = drone.Drone(nodes, x0, desired_trajectory)
+
+# Initialize Optimization:
+agent.initialize_optimization()
+
+# Update Constraints:
+agent.update_optimization()
+
+# Generate Trajectory:
+agent.generate_trajectory()
